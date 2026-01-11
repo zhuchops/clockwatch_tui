7342d0d57df7ca66ad1824082a2ee7d63c098ac0
zhuchops <zhuchok@duck.com>
2026-01-11 15:45:00 +0300

    added laps

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::{io, time::{self, Duration, Instant}};
 
+use color_eyre::owo_colors::OwoColorize;
 use ratatui::{DefaultTerminal, Frame, crossterm::event::{self, KeyCode, KeyEvent, KeyEventKind}, layout::{Constraint, Direction, Layout, Rect}, style::Stylize, text::{Line, Text, ToText}, widgets::{Block, Borders, Paragraph, Widget}};
 
 fn main() -> color_eyre::Result<()> {
@@ -42,8 +43,8 @@ impl App {
     }
 
     pub fn draw(&self, frame: &mut Frame) {
-        frame.render_widget(self, frame.area());
         frame.render_widget(&self.clock, frame.area());
+        frame.render_widget(self, frame.area());
     }
 
     pub fn handle_events(&mut self) -> io::Result<()> {
@@ -67,6 +68,10 @@ impl App {
                 self.clock.toggle_start_pause();
                 Ok(())
             }
+            KeyCode::Char('l') => {
+                self.clock.lap();
+                Ok(())
+            }
             _ => {Ok(())}
         }
     }
@@ -80,6 +85,8 @@ impl Widget for &App {
         let instructions = Line::from(vec![
             " Pause/Start ".into(),
             "<Space>".blue().bold(),
+            " Lap ".into(),
+            "<l>".blue().bold(),
             " Exit ".into(),
             "<q>".blue().bold(),
         ]).centered();
@@ -99,7 +106,7 @@ impl Widget for &App {
 struct Clockwatch {
     running: bool,
     elapsed_time: Duration, // accum time
-    laps: Vec<Instant>, // laps in seconds 
+    laps: Vec<Duration>, // laps in seconds 
 }
 
 impl Clockwatch {
@@ -114,33 +121,43 @@ impl Clockwatch {
     }
 
     fn lap(&mut self) {
-        todo!()
+        self.laps.push(self.elapsed_time);
+    }
+
+    fn duration_into_text(dt: Duration) -> String {
+        let all_millis = dt.as_millis();
+        let hours: u128 = all_millis / 1000 / 60 / 60;
+        let minutes: u128 = all_millis / 1000 / 60 % 60;
+        let secs: u128 = all_millis / 1000 % 60;
+        let millis: u128 = all_millis % 1000;
+        format!("{:02}:{:02}:{:02}:{:03}", hours, minutes, secs, millis)
     }
 }
 
 impl Widget for &Clockwatch {
     fn render(self, area: Rect, buf: &mut ratatui::prelude::Buffer) {
-        let all_millis = self.elapsed_time.as_millis();
-        let hours: u128 = all_millis / 1000 / 60 / 60;
-        let minutes: u128 = all_millis / 1000 / 60 % 60;
-        let secs: u128 = all_millis / 1000 % 60;
-        let millis: u128 = all_millis % 1000;
 
-        let clock_text = Text::from(vec![Line::from(format!(
-            "{:02}:{:02}:{:02}:{:03}",
-            hours, minutes, secs, millis
-        ))]);
+        let clock_text = Text::from(vec![Line::from(Clockwatch::duration_into_text(self.elapsed_time))]);
+
+        let mut laps_text = Text::from(vec![Line::from("Laps:")]);
+        for lap in &self.laps.iter().rev().collect::<Vec<&Duration>>() {
+            laps_text.push_line(Line::from(Clockwatch::duration_into_text(**lap)));
+        }
 
         let layout = Layout::default()
             .direction(Direction::Vertical)
             .constraints([
-                Constraint::Percentage(50),
-                Constraint::Min(1),
-                Constraint::Percentage(50),
+                Constraint::Percentage(30),
+                Constraint::Length(1),
+                Constraint::Min(0),
             ]).split(area);
 
         Paragraph::new(clock_text)
             .centered()
             .render(layout[1], buf);
+
+        Paragraph::new(laps_text)
+            .centered()
+            .render(layout[2], buf);
     }
 }
